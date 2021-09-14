bbca3decbd620b7f59eb8a7594f9f1302472fcb5
PramodRaoB <pramod.b@research.iiit.ac.in>
2021-09-14 23:11:46 +0530

    Fix echo bug for single word

--- commands/echo.c
+++ commands/echo.c
@@ -4,7 +4,7 @@
 int echo(list *tokens) {
     for (int i = 1; i < tokens->size - 1; i++)
         printf("%s ", tokens->arr[i]);
-    if (tokens->size > 2) printf("%s", tokens->arr[tokens->size - 1]);
+    if (tokens->size >= 2) printf("%s", tokens->arr[tokens->size - 1]);
     printf("\n");
     return 0;
 }
