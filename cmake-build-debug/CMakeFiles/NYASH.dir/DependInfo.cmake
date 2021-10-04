ae604b89c80fb5ccf8018b60f2f7d8c48544b6f8
PramodRaoB <pramod.b@research.iiit.ac.in>
2021-10-04 21:49:43 +0530

    Start phase 2 of shell making

---
+++ cmake-build-debug/CMakeFiles/NYASH.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/jade/CLionProjects/NYASH/commands/cd.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/commands/cd.c.o"
+  "/home/jade/CLionProjects/NYASH/commands/echo.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/commands/echo.c.o"
+  "/home/jade/CLionProjects/NYASH/commands/history.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/commands/history.c.o"
+  "/home/jade/CLionProjects/NYASH/commands/ls.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/commands/ls.c.o"
+  "/home/jade/CLionProjects/NYASH/commands/pinfo.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/commands/pinfo.c.o"
+  "/home/jade/CLionProjects/NYASH/commands/pwd.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/commands/pwd.c.o"
+  "/home/jade/CLionProjects/NYASH/commands/repeat.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/commands/repeat.c.o"
+  "/home/jade/CLionProjects/NYASH/main.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/main.c.o"
+  "/home/jade/CLionProjects/NYASH/processor/child_handler.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/processor/child_handler.c.o"
+  "/home/jade/CLionProjects/NYASH/processor/init.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/processor/init.c.o"
+  "/home/jade/CLionProjects/NYASH/processor/prompt.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/processor/prompt.c.o"
+  "/home/jade/CLionProjects/NYASH/processor/sysCommands.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/processor/sysCommands.c.o"
+  "/home/jade/CLionProjects/NYASH/utils/job_list.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/utils/job_list.c.o"
+  "/home/jade/CLionProjects/NYASH/utils/list.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/utils/list.c.o"
+  "/home/jade/CLionProjects/NYASH/utils/parse.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/utils/parse.c.o"
+  "/home/jade/CLionProjects/NYASH/utils/tokenize.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/utils/tokenize.c.o"
+  "/home/jade/CLionProjects/NYASH/utils/vector.c" "/home/jade/CLionProjects/NYASH/cmake-build-debug/CMakeFiles/NYASH.dir/utils/vector.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
